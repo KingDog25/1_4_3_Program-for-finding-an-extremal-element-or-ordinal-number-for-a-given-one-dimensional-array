8f59dd1701764be98f6888a5c079fb8423068b70
Alexandr Roslyakov <99288384+KingDog25@users.noreply.github.com>
2022-02-09 02:30:53 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿
+namespace _23_variant_CSarp_1_4_3
+{
+    partial class MinSum
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelMinSum = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.labelInputEl = new System.Windows.Forms.Label();
+            this.textBoxInputEl = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelMinSum
+            // 
+            this.labelMinSum.AutoSize = true;
+            this.labelMinSum.Font = new System.Drawing.Font("Segoe UI", 9.163636F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelMinSum.Location = new System.Drawing.Point(12, 416);
+            this.labelMinSum.Name = "labelMinSum";
+            this.labelMinSum.Size = new System.Drawing.Size(0, 19);
+            this.labelMinSum.TabIndex = 16;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-2, 114);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 47;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(803, 269);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // labelInputEl
+            // 
+            this.labelInputEl.AutoSize = true;
+            this.labelInputEl.Font = new System.Drawing.Font("Segoe UI", 9.163636F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.labelInputEl.Location = new System.Drawing.Point(-2, 16);
+            this.labelInputEl.Name = "labelInputEl";
+            this.labelInputEl.Size = new System.Drawing.Size(270, 19);
+            this.labelInputEl.TabIndex = 14;
+            this.labelInputEl.Text = "Введите(через пробел) аргументы X:";
+            // 
+            // textBoxInputEl
+            // 
+            this.textBoxInputEl.Location = new System.Drawing.Point(274, 13);
+            this.textBoxInputEl.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBoxInputEl.Name = "textBoxInputEl";
+            this.textBoxInputEl.Size = new System.Drawing.Size(290, 26);
+            this.textBoxInputEl.TabIndex = 13;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(593, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 26);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Применить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // MinSum
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 484);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.labelMinSum);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.labelInputEl);
+            this.Controls.Add(this.textBoxInputEl);
+            this.Name = "MinSum";
+            this.Text = "Программа нахождения экстремального элемента";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelMinSum;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label labelInputEl;
+        private System.Windows.Forms.TextBox textBoxInputEl;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿/*Условие: Составить программу нахождения экстремального элемента
+или его порядкового номера при заданном одномерном массиве А из n элементов*/
+/**Задача 1.4.3 Условие:Определить найменьшее значение сумм Ai  + Ai - 1*/
+
+/*Condition: Write a program for finding an extremal element
+or its ordinal number for a given one-dimensional array A of n elements */
+/**Task 1.4.3 Condition: Determine the smallest value of the sums Ai + Ai - 1*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _23_variant_CSarp_1_4_3
+{
+    public partial class MinSum : Form
+    {
+        public MinSum()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] param = textBoxInputEl.Text.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int n = param.Length;                                          //кол-во эл. массива
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(param[i]);
+            }
+            double[,] tableXY = new double[1, n];
+            dataGridView1.RowCount = 1;                             //кол-во строк
+            dataGridView1.ColumnCount = n;                          //столбцов
+            dataGridView1.Rows[0].HeaderCell.Value = "Введен массив: ";
+            int minsum = 0;                                           //кол-во полож. значений
+
+            for (int j = 0; j < n; j++)
+            {
+                tableXY[0, j] = arr[j];
+                dataGridView1.Rows[0].Cells[j].Value = tableXY[0, j];
+            }
+            //int[] arrA = new int[n];
+            int rightEl = n - 1, leftEl = n - 2;                      //счетчики индексов для массива справа налево
+            minsum = arr[rightEl] + arr[leftEl];                     // Присваиваем переменной сумму последних двух элементов массива
+            for (int i = n - 1; i > 0; i--)
+            {
+                if (arr[i] + arr[i - 1] < minsum)                     //Если их сумма  < текущего, то перезаписываем переменную
+                {
+                    minsum = arr[i] + arr[i - 1];                     //запоминаем индексы
+                    rightEl = i; leftEl = i - 1;
+                }
+            }
+                                                                       //добавляем фон элементам
+            dataGridView1.Rows[0].Cells[rightEl].Style.BackColor = Color.Green;   
+            dataGridView1.Rows[0].Cells[leftEl].Style.BackColor = Color.Green;
+            labelMinSum.Text = "Найменьшее значение сумм Ai + Ai - 1 = " + minsum.ToString();
+            minsum = 0;                                                 //сбросить счетчик
+            
+        }
+    }
+}
